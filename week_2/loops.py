2b6a93dcb7a73ff1824ae1c11c2c510068ceb562
janegitata6@gmail.com <ivychege33@gmail.com>
2024-02-21 23:15:01 -0800

    strings ,loops

---
+++ week_2/loops.py
@@ -0,0 +1,18 @@
+#loops and iterations
+#Date:22/2/2024
+#Name: Jane
+
+
+#for loop
+#while loop
+for number in range(0,10):
+    
+    print(str(number)+"\t",end=" ")
+
+    
+
+
+    #squares of number
+for x  in range(0,11):
+    print(str(x**2 )+ "\t",end=" ") # squares
+    
---
+++ week_2/strings.py
@@ -0,0 +1,50 @@
+#stings in python
+#Date:22/2/2024
+#Name:Jane
+
+city = "nairobi"
+
+#convert to  uppercase
+print(city[0])#prints n
+print(city[1])#prints a
+print(city[2])#prints i
+print(city[3])#prints r
+print(city[4])#prints o
+print(city[5])#prints b
+print(city[6])#prints i
+
+print(city[-1])#prints reverse starting with i
+print(city[-2])#prints reverse starting with b
+
+#replacing a character
+fruit= "OrangeOOOO"
+
+print(fruit.replace("O" ,"Y"))
+
+SUBJECT ="Phyical :Sciences"
+print(SUBJECT.split(":"))
+
+
+
+print("\n") #prints a new line
+print(city.upper())
+name ="JANE"
+print(name)
+print(name.lower()) # converts strings to lower case
+
+town = "   Naivasha   "
+print(town)
+print("\t") # new tab
+print(town.strip())
+
+#add  two strings
+
+f_name = "Jane"
+s_name = "Njeri"
+full_name =f_name + s_name
+
+
+print(full_name)
+age  =30
+height = 1.2
+print("I am age{0} years old and {1}meters tall".format(age,height))
